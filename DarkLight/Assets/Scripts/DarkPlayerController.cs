93cf6ca7a14c5c2dd0cc1a5fe0f521f1a81aecbd
ccortizt <ccamilo0203@gmail.com>
2017-03-01 13:22:24 -0500

    Base scene and player added

---
+++ DarkLight/Assets/Scripts/DarkPlayerController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class DarkPlayerController : MonoBehaviour
+{
+
+    int teleportDistance;
+
+    float flashCoolDown = 3;
+    bool isInCoolDown;
+    float flashCoolDownCount;
+
+
+    void Start()
+    {
+        flashCoolDownCount = 0;
+        teleportDistance = 4;
+        isInCoolDown = false;
+    }
+
+    void Update()
+    {
+
+        if (flashCoolDownCount > 0 && isInCoolDown)
+        {
+            flashCoolDownCount -= Time.deltaTime;
+        }
+        else if (flashCoolDownCount <= 0)
+        {
+            isInCoolDown = false;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+            if (Input.GetKey(KeyCode.Space) && !isInCoolDown)
+            {
+                transform.position = transform.position - teleportDistance * transform.right;
+                SetCoolDown();
+            }
+            else
+            {
+                transform.position = transform.position - transform.right;
+            }
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (Input.GetKey(KeyCode.Space) && !isInCoolDown)
+            {
+
+                transform.position = transform.position + teleportDistance * transform.right;
+                SetCoolDown();
+            }
+            else
+            {
+                transform.position = transform.position + transform.right;
+            }
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (Input.GetKey(KeyCode.Space) && !isInCoolDown)
+            {
+                transform.position = transform.position + teleportDistance * transform.forward;
+                SetCoolDown();
+            }
+            else
+            {
+                transform.position = transform.position + transform.forward;
+            }
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (Input.GetKey(KeyCode.Space) && !isInCoolDown)
+            {
+                transform.position = transform.position - teleportDistance * transform.forward;
+                SetCoolDown();
+            }
+            else
+            {
+                transform.position = transform.position - transform.forward;
+            }
+
+        }
+
+    }
+
+    private void SetCoolDown()
+    {
+        flashCoolDownCount = flashCoolDown;
+        isInCoolDown = true;
+
+    }
+
+}
