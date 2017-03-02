8d76487104a4b731a8b13ecf57e5295996843364
ccortizt <ccamilo0203@gmail.com>
2017-03-01 19:37:51 -0500

    Hud Added, scenario lighting changes, enrgy and skill scripts created

---
+++ DarkLight/Assets/Scripts/PlayerEnergyController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerEnergyController : MonoBehaviour {
+
+
+    private int energyLevel;
+    private int initialEnergyLevel = 100;
+
+    private Image energyBar;
+
+	void Start () {
+
+        energyBar = GameObject.Find("PlayerHUD").transform.FindChild("LifePanel/EneryPanel/Panel/Energy").GetComponent<Image>();
+        energyLevel = initialEnergyLevel;
+	}
+
+    public void AddEnergy(int quantity)
+    {
+        if (energyLevel < initialEnergyLevel)
+        {
+            if (energyLevel + quantity > initialEnergyLevel)
+                energyLevel = initialEnergyLevel;
+            else
+                energyLevel += quantity;
+
+            UpdateEnergyBar();
+        }
+    }
+
+    public void DecreaseEnergy(int quantity)
+    {
+        if (energyLevel > 0)
+        {
+            energyLevel -= quantity;
+
+            UpdateEnergyBar();
+        }
+    }
+	
+	private void UpdateEnergyBar ()
+    {
+        energyBar.fillAmount = (float)energyLevel / (float)initialEnergyLevel;
+        //Debug.Log("energy: " + energyLevel);
+
+    }
+
+    public int GetEnergyLevel()
+    {
+        return energyLevel;
+    }
+}
