7369a03e0fda2143ff29f5280c315487106fce8b
ccortizt <ccamilo0203@gmail.com>
2017-03-12 20:04:59 -0500

    Level Creator added, destroy platforms mecanic added

--- DarkLight/Assets/Scripts/LevelCreator.cs
+++ DarkLight/Assets/Scripts/LevelCreator.cs
@@ -1,7 +1,100 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
+using System.Collections;
 using UnityEngine;
 public class LevelCreator: MonoBehaviour{
-    //y - 0.15 || y + 0.35
-    //x + .8 singles!!
+    
+    public GameObject prefabPlatform;
+    public GameObject prefabDoublePlatform;
+    public GameObject prefabDestroyPlatform;
+
+    private float minBaseValueX = 4.35f;
+    private float maxBaseValueX = 4.55f;
+
+    private int minPlatformsPerY = 2;
+    private int maxPlatformsPerY = 6;
+
+    //private float baseValueX = 4.5f;
+
+    private float minBaseValueY = 0.4f;
+    private float maxBaseValueY = 0.7f;
+
+    private float platformSizePercentageChooser = 4;
+
+    private float waitDestroyTime = 3;
+
+    private bool[,] platformMap = new bool[50, 10];
+
+
+    void Start()
+    {
+        InitializePlatformMap();
+        FillMap();
+        StartCoroutine(DestroyMap());
+    }
+
+    private void InitializePlatformMap()
+    {
+        for (int i = 0; i < 50; i++)
+            for (int j = 0; j < 10; j++)
+                platformMap[i, j] = false;
+    }
+
+    private void FillMap()
+    {
+        int numberPlatformsY;
+
+        for (int i = 0; i < 50; i+=2)
+        {
+            numberPlatformsY = RandomNumberPlatformAxisY();
+            
+            for (int j = 0; j < 10; j++)
+            {
+                if(Random.Range(0,10) < numberPlatformsY)
+                    if(RandomPlatformSize())
+                        Instantiate(prefabPlatform, new Vector3(j - RandomPositionX(), i + RandomPositionY(), 0), Quaternion.identity);
+                    else
+                        Instantiate(prefabDoublePlatform, new Vector3(j - RandomPositionX(), i + RandomPositionY(), 0), Quaternion.identity);
+            }
+            
+        }
+    }
+
+    public IEnumerator DestroyMap()
+    {
+        for (int i = 1; i < 50; i+=2)
+        {
+            yield return new WaitForSeconds(waitDestroyTime);
+            Instantiate(prefabDestroyPlatform, new Vector3(RandomDestroyPlatformSide() * 10, i - 1f, 0), Quaternion.identity);
+        }
+    }
+
+    private float RandomPositionY()
+    {
+        return Random.Range(minBaseValueY,maxBaseValueY);
+    }
+
+    private float RandomPositionX()
+    {
+        return Random.Range(minBaseValueX, maxBaseValueX);
+    }
+
+    private int RandomNumberPlatformAxisY()
+    {
+        return Random.Range(minPlatformsPerY, maxPlatformsPerY);
+    }
+
+    private bool RandomPlatformSize()
+    {
+        return Random.Range(0, 10) > platformSizePercentageChooser;
+    }
+
+    private int RandomDestroyPlatformSide()
+    {
+        if (Random.Range(0, 10) >= 5)
+        {
+            return -1;
+        } else 
+            return 1;
+    }
+
 }
---
+++ DarkLight/Assets/Scripts/PlatformDestroyController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlatformDestroyController: MonoBehaviour{
+
+    public bool positiveSide;
+
+    private float proportion = 0.02f;
+
+    private bool stop;
+    void Start()
+    {
+        stop = false;
+        if (transform.position.x > 0)
+        {
+            positiveSide = true;
+        }
+        else
+        {
+            positiveSide = false;
+        }
+    }
+
+    void Update()
+    {
+
+        if (!stop)
+        {
+            CheckStop();
+            MoveAndDestroy();
+        }            
+        
+    }
+
+    private void CheckStop()
+    {
+        if (transform.position.x >= -0.005 && transform.position.x <= 0.005)
+        {
+            stop = true;
+        }
+    }
+
+    void MoveAndDestroy()
+    {
+        if (positiveSide)
+        {
+            transform.position += new Vector3(-proportion, 0, 0);
+        }
+
+        else
+        {
+            transform.position += new Vector3(proportion, 0, 0);
+        }
+    }
+}
